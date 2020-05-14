e773ada6b32b1aa96ed5b6d679f6fa9cd6a8f83e
indigodarkwolf <indigo.darkwolf@gmail.com>
2020-05-13 21:05:27 -0500

    Applying irq draw, gnuc fix, sprite collide

--- video.c
+++ video.c
@@ -93,8 +93,11 @@ static uint8_t reg_composer[8];
 static uint8_t layer_line[2][SCREEN_WIDTH];
 static uint8_t sprite_line_col[SCREEN_WIDTH];
 static uint8_t sprite_line_z[SCREEN_WIDTH];
-static bool layer_line_empty[2];
+static uint8_t sprite_line_mask[SCREEN_WIDTH];
+static uint8_t irq_mask[SCREEN_WIDTH];
+static bool    layer_line_empty[2];
 static bool sprite_line_empty;
+static uint8_t sprite_line_collisions;
 
 float scan_pos_x;
 uint16_t scan_pos_y;
@@ -102,6 +105,8 @@ uint16_t scan_pos_y;
 static uint8_t framebuffer[SCREEN_WIDTH * SCREEN_HEIGHT * 4];
 
 static GifWriter gif_writer;
+
+bool overwrite_during_irqs = false;
 
 static const uint16_t default_palette[] = {
 0x000,0xfff,0x800,0xafe,0xc4c,0x0c5,0x00a,0xee7,0xd85,0x640,0xf77,0x333,0x777,0xaf6,0x08f,0xbbb,0x000,0x111,0x222,0x333,0x444,0x555,0x666,0x777,0x888,0x999,0xaaa,0xbbb,0xccc,0xddd,0xeee,0xfff,0x211,0x433,0x644,0x866,0xa88,0xc99,0xfbb,0x211,0x422,0x633,0x844,0xa55,0xc66,0xf77,0x200,0x411,0x611,0x822,0xa22,0xc33,0xf33,0x200,0x400,0x600,0x800,0xa00,0xc00,0xf00,0x221,0x443,0x664,0x886,0xaa8,0xcc9,0xfeb,0x211,0x432,0x653,0x874,0xa95,0xcb6,0xfd7,0x210,0x431,0x651,0x862,0xa82,0xca3,0xfc3,0x210,0x430,0x640,0x860,0xa80,0xc90,0xfb0,0x121,0x343,0x564,0x786,0x9a8,0xbc9,0xdfb,0x121,0x342,0x463,0x684,0x8a5,0x9c6,0xbf7,0x120,0x241,0x461,0x582,0x6a2,0x8c3,0x9f3,0x120,0x240,0x360,0x480,0x5a0,0x6c0,0x7f0,0x121,0x343,0x465,0x686,0x8a8,0x9ca,0xbfc,0x121,0x242,0x364,0x485,0x5a6,0x6c8,0x7f9,0x020,0x141,0x162,0x283,0x2a4,0x3c5,0x3f6,0x020,0x041,0x061,0x082,0x0a2,0x0c3,0x0f3,0x122,0x344,0x466,0x688,0x8aa,0x9cc,0xbff,0x122,0x244,0x366,0x488,0x5aa,0x6cc,0x7ff,0x022,0x144,0x166,0x288,0x2aa,0x3cc,0x3ff,0x022,0x044,0x066,0x088,0x0aa,0x0cc,0x0ff,0x112,0x334,0x456,0x668,0x88a,0x9ac,0xbcf,0x112,0x224,0x346,0x458,0x56a,0x68c,0x79f,0x002,0x114,0x126,0x238,0x24a,0x35c,0x36f,0x002,0x014,0x016,0x028,0x02a,0x03c,0x03f,0x112,0x334,0x546,0x768,0x98a,0xb9c,0xdbf,0x112,0x324,0x436,0x648,0x85a,0x96c,0xb7f,0x102,0x214,0x416,0x528,0x62a,0x83c,0x93f,0x102,0x204,0x306,0x408,0x50a,0x60c,0x70f,0x212,0x434,0x646,0x868,0xa8a,0xc9c,0xfbe,0x211,0x423,0x635,0x847,0xa59,0xc6b,0xf7d,0x201,0x413,0x615,0x826,0xa28,0xc3a,0xf3c,0x201,0x403,0x604,0x806,0xa08,0xc09,0xf0b
@@ -153,6 +158,8 @@ video_reset()
 	for (int i = 0; i < 128 * 1024; i++) {
 		video_ram[i] = rand();
 	}
+
+	sprite_line_collisions = 0;
 
 	scan_pos_x = 0;
 	scan_pos_y = 0;
@@ -341,6 +348,7 @@ void refresh_layer_properties(uint8_t layer)
 struct video_sprite_properties
 {
 	int8_t sprite_zdepth;
+	uint8_t sprite_collision_mask;
 
 	int16_t sprite_x;
 	int16_t sprite_y;
@@ -363,6 +371,7 @@ void refresh_sprite_properties(uint16_t sprite)
 	struct video_sprite_properties* props = &sprite_properties[sprite];
 
 	props->sprite_zdepth = (sprite_data[sprite][6] >> 2) & 3;
+	props->sprite_collision_mask = sprite_data[sprite][6] & 0xf0;
 
 	props->sprite_x = sprite_data[sprite][2] | (sprite_data[sprite][3] & 3) << 8;
 	props->sprite_y = sprite_data[sprite][4] | (sprite_data[sprite][5] & 3) << 8;
@@ -434,6 +443,7 @@ render_sprite_line(uint16_t y)
 	for (int i = 0; i < SCREEN_WIDTH; i++) {
 		sprite_line_col[i] = 0;
 		sprite_line_z[i] = 0;
+		sprite_line_mask[i] = 0;
 	}
 	uint16_t sprite_budget = 800 + 1;
 	for (int i = 0; i < NUM_SPRITES; i++) {
@@ -492,6 +502,10 @@ render_sprite_line(uint16_t y)
 			// palette offset
 			if (col_index > 0) {
 				col_index += props->palette_offset;
+
+				sprite_line_collisions |= sprite_line_mask[line_x] & props->sprite_collision_mask;
+				sprite_line_mask[line_x] |= props->sprite_collision_mask;
+
 				if (props->sprite_zdepth > sprite_line_z[line_x]) {
 					sprite_line_col[line_x] = col_index;
 					sprite_line_z[line_x] = props->sprite_zdepth;
@@ -630,16 +644,16 @@ static uint8_t calculate_line_col_index(uint8_t spr_zindex, uint8_t spr_col_inde
 	uint8_t col_index = 0;
 	switch (spr_zindex) {
 		case 3:
-			col_index = spr_col_index ?: l2_col_index ?: l1_col_index;
+			col_index = spr_col_index ? spr_col_index : (l2_col_index ? l2_col_index : l1_col_index);
 			break;
 		case 2:
-			col_index = l2_col_index ?: spr_col_index ?: l1_col_index;
+			col_index = l2_col_index ? l2_col_index : (spr_col_index ? spr_col_index : l1_col_index);
 			break;
 		case 1:
-			col_index = l2_col_index ?: l1_col_index ?: spr_col_index;
+			col_index = l2_col_index ? l2_col_index : (l1_col_index ? l1_col_index : spr_col_index);
 			break;
 		case 0:
-			col_index = l2_col_index ?: l1_col_index;
+			col_index = l2_col_index ? l2_col_index : l1_col_index;
 			break;
 	}
 	return col_index;
@@ -720,22 +734,22 @@ render_line(uint16_t y)
 				switch (spr_zindex[0]) {
 					case 3:
 						for (int i = 0; i < LAYER_PIXELS_PER_ITERATION; ++i) {
-							col_index[i] = spr_col_index[i] ?: l2_col_index[i] ?: l1_col_index[i];
+							col_index[i] = spr_col_index[i] ? spr_col_index[i] : (l2_col_index[i] ? l2_col_index[i] : l1_col_index[i]);
 						}
 						break;
 					case 2:
 						for (int i = 0; i < LAYER_PIXELS_PER_ITERATION; ++i) {
-							col_index[i] = l2_col_index[i] ?: spr_col_index[i] ?: l1_col_index[i];
+							col_index[i] = l2_col_index[i] ? l2_col_index[i] : (spr_col_index[i] ? spr_col_index[i] : l1_col_index[i]);
 						}
 						break;
 					case 1:
 						for (int i = 0; i < LAYER_PIXELS_PER_ITERATION; ++i) {
-							col_index[i] = l2_col_index[i] ?: l1_col_index[i] ?: spr_col_index[i];
+							col_index[i] = l2_col_index[i] ? l2_col_index[i] : (l1_col_index[i] ? l1_col_index[i] : spr_col_index[i]);
 						}
 						break;
 					case 0:
 						for (int i = 0; i < LAYER_PIXELS_PER_ITERATION; ++i) {
-							col_index[i] = l2_col_index[i] ?: l1_col_index[i];
+							col_index[i] = l2_col_index[i] ? l2_col_index[i] : l1_col_index[i];
 						}
 						break;
 				}
@@ -765,7 +779,7 @@ render_line(uint16_t y)
 	{
 		uint32_t* framebuffer4 = framebuffer4_begin;
 		for (uint16_t x = 0; x < SCREEN_WIDTH; x++) {
-			*framebuffer4++ = video_palette.entries[col_line[x]];
+			*framebuffer4++ = irq_mask[x] ? 0x0000ff00 : video_palette.entries[col_line[x]];
 		}
 	}
 
@@ -796,6 +810,31 @@ video_step(float mhz)
 	bool new_frame = false;
 	float advance = ((out_mode & 2) ? NTSC_PIXEL_FREQ :  VGA_PIXEL_FREQ) / mhz;
 	scan_pos_x += advance;
+
+	if (overwrite_during_irqs) {
+		uint16_t front_porch_y = (out_mode & 2) ? NTSC_FRONT_PORCH_Y : VGA_FRONT_PORCH_Y;
+		uint16_t front_porch_x = (out_mode & 2) ? NTSC_FRONT_PORCH_X : VGA_FRONT_PORCH_X;
+
+		if ((scan_pos_x >= front_porch_x) && (scan_pos_y >= front_porch_y)) {
+			uint16_t current_x = scan_pos_x - front_porch_x;
+			static uint16_t last_x    = 0;
+
+			if (current_x < last_x) {
+				last_x = 0;
+			}
+
+			if (current_x > SCREEN_WIDTH) {
+				current_x = SCREEN_WIDTH;
+			}
+
+			for (uint16_t x = last_x; x < current_x; x++) {
+				irq_mask[x] = status & 0x04;
+			}
+
+			last_x = current_x;
+		}
+	}
+
 	if (scan_pos_x > SCAN_WIDTH) {
 		scan_pos_x -= SCAN_WIDTH;
 		uint16_t front_porch = (out_mode & 2) ? NTSC_FRONT_PORCH_Y : VGA_FRONT_PORCH_Y;
@@ -804,6 +843,15 @@ video_step(float mhz)
 			render_line(y);
 		}
 		scan_pos_y++;
+		if (scan_pos_y == SCREEN_HEIGHT) {
+			if (ien & 4) {
+				if (sprite_line_collisions != 0) {
+					isr |= 4;
+				}
+				isr = (isr & 0xf) | sprite_line_collisions;
+			}
+			sprite_line_collisions = 0;
+		}
 		if (scan_pos_y == SCAN_HEIGHT) {
 			scan_pos_y = 0;
 			new_frame = true;
@@ -1051,7 +1099,7 @@ uint8_t video_read(uint8_t reg, bool debugOn) {
 			return value;
 		}
 		case 0x05: return (io_dcsel << 1) | io_addrsel;
-		case 0x06: return ((irq_line & 1) << 7) | (ien & 0xF);
+		case 0x06: return ((irq_line & 0x0100) >> 1) | (ien & 0xF);
 		case 0x07: return isr | (pcm_is_fifo_almost_empty() ? 8 : 0);
 		case 0x08: return irq_line & 0xFF;
 
@@ -1131,6 +1179,7 @@ void video_write(uint8_t reg, uint8_t value) {
 			isr &= value ^ 0xff;
 			break;
 		case 0x08:
+			irq_line = (irq_line & 0x100) | value;
 			break;
 
 		case 0x09:
